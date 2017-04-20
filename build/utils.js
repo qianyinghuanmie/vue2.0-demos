0ab7b95b775e3e3aa1d1d60c13ba1b79b0f1b09e
root <1395315573@qq.com>
2017-04-20 16:00:50 +0800

    first commit

---
+++ build/utils.js
@@ -0,0 +1,98 @@
+var path = require('path')
+var config = require('../config')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var glob = require('glob');
+exports.assetsPath = function (_path) {
+  var assetsSubDirectory = process.env.NODE_ENV === 'production'
+    ? config.build.assetsSubDirectory
+    : config.dev.assetsSubDirectory
+  return path.posix.join(assetsSubDirectory, _path)
+}
+
+exports.cssLoaders = function (options) {
+  options = options || {}
+
+  var cssLoader = {
+    loader: 'css-loader',
+    options: {
+      minimize: process.env.NODE_ENV === 'production',
+      sourceMap: options.sourceMap
+    }
+  }
+
+  // generate loader string to be used with extract text plugin
+  function generateLoaders (loader, loaderOptions) {
+    var loaders = [cssLoader]
+    if (loader) {
+      loaders.push({
+        loader: loader + '-loader',
+        options: Object.assign({}, loaderOptions, {
+          sourceMap: options.sourceMap
+        })
+      })
+    }
+
+    // Extract CSS when that option is specified
+    // (which is the case during production build)
+    if (options.extract) {
+      return ExtractTextPlugin.extract({
+        use: loaders,
+        fallback: 'vue-style-loader'
+      })
+    } else {
+      return ['vue-style-loader'].concat(loaders)
+    }
+  }
+
+  // https://vue-loader.vuejs.org/en/configurations/extract-css.html
+  return {
+    css: generateLoaders(),
+    postcss: generateLoaders(),
+    less: generateLoaders('less'),
+    sass: generateLoaders('sass', { indentedSyntax: true }),
+    scss: generateLoaders('sass'),
+    stylus: generateLoaders('stylus'),
+    styl: generateLoaders('stylus')
+  }
+}
+
+// Generate loaders for standalone style files (outside of .vue)
+exports.styleLoaders = function (options) {
+  var output = []
+  var loaders = exports.cssLoaders(options)
+  for (var extension in loaders) {
+    var loader = loaders[extension]
+    output.push({
+      test: new RegExp('\\.' + extension + '$'),
+      use: loader
+    })
+  }
+  return output
+}
+/*用于多页面的模板*/
+exports.getEntries = function (globPath) {
+  var entries = {}
+  /**
+   * 读取src目录,并进行路径裁剪
+   */
+  glob.sync(globPath).forEach(function (entry) {
+    /**
+     * path.basename 提取出用 ‘/' 隔开的path的最后一部分，除第一个参数外其余是需要过滤的字符串
+     * path.extname 获取文件后缀
+     */
+    var basename = path.basename(entry, path.extname(entry), 'router.js') // 过滤router.js
+    // ***************begin***************
+    // 当然， 你也可以加上模块名称, 即输出如下： { module/main: './src/module/index/index.js', module/test: './src/module/test/test.js' }
+    // 最终编译输出的文件也在module目录下， 访问路径需要时 localhost:8080/module/index.html
+    // slice 从已有的数组中返回选定的元素, -3 倒序选择，即选择最后三个
+    // var tmp = entry.split('/').splice(-3)
+    // var pathname = tmp.splice(0, 1) + '/' + basename; // splice(0, 1)取tmp数组中第一个元素
+    // console.log(pathname)
+    // entries[pathname] = entry
+    // ***************end***************
+    entries[basename] = entry
+  });
+  // console.log(entries);
+  // 获取的主入口如下： { main: './src/module/index/index.js', test: './src/module/test/test.js' }
+  return entries;
+}
---
+++ src/components/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <a href="./index.html">从about跳转到demo1</a>
+  </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style>
+
+</style>
